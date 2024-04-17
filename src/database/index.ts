746097ba0d958e56205f6eba8461402d5c6f3c82
Patrick Cuppi <cuppi.patrick@gmail.com>
2024-04-17 12:34:46 -0300

    feat: added sequelize connection

---
+++ src/database/index.ts
@@ -0,0 +1,11 @@
+import { Sequelize } from 'sequelize';
+
+export const sequelize = new Sequelize({
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'onebitflix_development',
+    username: 'onebitflix',
+    password: 'onebitflix'
+})
+
