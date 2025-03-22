debb14402ef9ef0cf4f5387114a59c82a67c7faa
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-22 19:54:44 -0300

    :wrench: chore: create database (postgres)

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,14 +1,13 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize'
 
 export const sequelize = new Sequelize({
-    dialect: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    database: 'onebitflix_development',
-    username: 'onebitflix',
-    password: 'onebitflix',
-    define: {
-        underscored: true
-    }
+  dialect: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  database: 'onebitflix_development',
+  username: 'onebitflix',
+  password: 'onebitflix',
+  define: {
+    underscored: true,
+  },
 })
-
