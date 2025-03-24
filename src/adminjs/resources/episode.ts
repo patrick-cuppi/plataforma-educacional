d77b2c24759c92b3f896cfa81ca93cf625687f58
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-24 17:57:08 -0300

    :card_file_box: feat: create migration and episodes model

---
+++ src/adminjs/resources/episode.ts
@@ -0,0 +1,19 @@
+import type { ResourceOptions } from 'adminjs'
+
+export const episodeResourceOptions: ResourceOptions = {
+  navigation: 'Catálogo',
+  editProperties: ['name', 'synopsis', 'courseId', 'order', 'uploadVideo', 'secondsLong'],
+  filterProperties: ['name', 'synopsis', 'courseId', 'secondsLong', 'createdAt', 'updatedAt'],
+  listProperties: ['id', 'name', 'courseId', 'order', 'secondsLong'],
+  showProperties: [
+    'id',
+    'name',
+    'synopsis',
+    'courseId',
+    'order',
+    'videoUrl',
+    'secondsLong',
+    'createdAt',
+    'updatedAt',
+  ],
+}
