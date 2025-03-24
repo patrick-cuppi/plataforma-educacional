41409893b26432332313ffa588a633e63f1effdd
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-24 15:57:29 -0300

    :recycle: fix: divergent navigation titles

--- src/adminjs/resources/category.ts
+++ src/adminjs/resources/category.ts
@@ -1,7 +1,7 @@
 import type { ResourceOptions } from 'adminjs'
 
 export const categortyResourceOptions: ResourceOptions = {
-  navigation: 'Catálago',
+  navigation: 'Catálogo',
   editProperties: ['name', 'position'],
   filterProperties: ['name', 'position', 'createdAt', 'updatedAt'],
   listProperties: ['id', 'name', 'position'],
--- src/adminjs/resources/index.ts
+++ src/adminjs/resources/index.ts
@@ -1,10 +1,15 @@
 import type { ResourceWithOptions } from 'adminjs'
-import { Category } from '../../models'
+import { Category, Course } from '../../models'
 import { categortyResourceOptions } from './category'
+import { courseResourceOptions } from './course'
 
 export const adminJsResource: ResourceWithOptions[] = [
   {
     resource: Category,
     options: categortyResourceOptions,
+  },
+  {
+    resource: Course,
+    options: courseResourceOptions,
   },
 ]
