e70d8e89533b7cca8574995f40eb9f6bf7ba2d8b
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-26 17:37:24 -0300

    feat(controller): custom sequence list of all categories

--- src/@controller/categories-controller.ts
+++ src/@controller/categories-controller.ts
@@ -3,7 +3,10 @@ import { Category } from '../models'
 
 export const categoriesController = {
   index: async (req: Request, res: Response) => {
-    const categories = await Category.findAll()
+    const categories = await Category.findAll({
+      attributes: ['id', 'name', 'position'],
+      order: [['position', 'ASC']],
+    })
 
     return res.json(categories)
   },
