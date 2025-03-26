59c93529da8fae5b7a73547532271cc2333d8847
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-26 17:44:32 -0300

    refactor(controller): refined code

--- src/@controller/categories-controller.ts
+++ src/@controller/categories-controller.ts
@@ -3,11 +3,17 @@ import { Category } from '../models'
 
 export const categoriesController = {
   index: async (req: Request, res: Response) => {
-    const categories = await Category.findAll({
-      attributes: ['id', 'name', 'position'],
-      order: [['position', 'ASC']],
-    })
+    try {
+      const categories = await Category.findAll({
+        attributes: ['id', 'name', 'position'],
+        order: [['position', 'ASC']],
+      })
 
-    return res.json(categories)
+      return res.json(categories)
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message })
+      }
+    }
   },
 }
