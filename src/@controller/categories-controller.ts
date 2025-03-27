4e5ca506f890b80ee27467de09aa201c30ad04a4
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-27 16:38:21 -0300

    refactor(controller): pagination included - all categories

--- src/@controller/categories-controller.ts
+++ src/@controller/categories-controller.ts
@@ -1,20 +1,13 @@
-import type { Request, Response } from 'express'
+import { type Request, type Response, query } from 'express'
+import { getPaginationParams } from '../@helpers/getPaginationParams'
 import { categoryService } from '../@services/categoryService'
 
 export const categoriesController = {
   index: async (req: Request, res: Response) => {
-    const { page, perPage } = req.query
-
-    const perPageNumber =
-      typeof perPage === 'string' && Number.parseInt(perPage, 10) > 0
-        ? Number.parseInt(perPage, 10)
-        : 10
-
-    const pageNumber =
-      typeof page === 'string' && Number.parseInt(page, 10) > 0 ? Number.parseInt(page, 10) : 1
+    const [page, perPage] = getPaginationParams(req.query)
 
     try {
-      const paginatedCategories = await categoryService.findAllPaginated(pageNumber, perPageNumber)
+      const paginatedCategories = await categoryService.findAllPaginated(page, perPage)
 
       return res.json(paginatedCategories)
     } catch (error) {
