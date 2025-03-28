c1e76bc18d525da89ace4c3b625b45ef672ee52a
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-03-28 19:16:13 -0300

    feat(controller): get 3 top courses

--- src/@controller/coursesController.ts
+++ src/@controller/coursesController.ts
@@ -2,6 +2,18 @@ import type { Request, Response } from 'express'
 import { courseService } from '../@services/courseService'
 
 export const coursesController = {
+  featured: async (req: Request, res: Response) => {
+    try {
+      const featuredCourses = await courseService.getRandomFeaturedCourses()
+
+      return res.json(featuredCourses)
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message })
+      }
+    }
+  },
+
   show: async (req: Request, res: Response) => {
     const { id } = req.params
 
