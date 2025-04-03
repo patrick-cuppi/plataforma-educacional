125d64098ea4e8f8b25997c825cdd0558770d344
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-04-03 14:41:52 -0300

    feat: create endpoint like

---
+++ src/@controller/likesController.ts
@@ -0,0 +1,21 @@
+import type { Response } from 'express'
+import type { AuthenticatedRequest } from '../@middlewares/auth'
+import { likeService } from '../@services/likeService'
+
+export const likesController = {
+  save: async (req: AuthenticatedRequest, res: Response) => {
+    // biome-ignore lint/style/noNonNullAssertion: <explanation>
+    const userId = req.user!.id
+    const { courseId } = req.body
+
+    try {
+      const like = await likeService.create(userId, courseId)
+
+      return res.status(201).json(like)
+    } catch (error) {
+      if (error instanceof Error) {
+        return res.status(400).json({ message: error.message })
+      }
+    }
+  },
+}
